87380bf2aa9e580abdc110a8db2ad845f6c09451
Kevin J <7211424+Xmaxer@users.noreply.github.com>
2020-04-13 09:57:47 -0700

    add drop database to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-rails db:create db:migrate
+rails db:drop db:create db:migrate
 bundle exec rails s -b 0.0.0.0
 bundle exec sidekiq
